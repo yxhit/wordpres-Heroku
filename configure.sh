4a15765d98f3e9fbf04bccd979eccf3d9140ee4e
x-potato <50793719+yxhit@users.noreply.github.com>
2020-12-21 22:03:37 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-# Download and install V2Ray
-mkdir /tmp/Xray
-curl -L -H "Cache-Control: no-cache" -o /tmp/Xray/Xray.zip https://github.com/XTLS/Xray-core/releases/download/$RELEASE_VERSION/Xray-linux-$MACHINE.zip
-unzip /tmp/Xray/Xray.zip -d /tmp/Xray
-install -m 755 /tmp/Xray/Xray /usr/local/bin/Xray
-install -m 755 /tmp/Xray/Xctl /usr/local/bin/Xctl
+# Download and install xRay
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/$RELEASE_VERSION/Xray-linux-$MACHINE.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
 
 # Remove temporary directory
-rm -rf /tmp/Xray
+rm -rf /tmp/xray
 
 # XRay new configuration
-install -d /usr/local/etc/Xray
-cat << EOF > /usr/local/etc/Xray/config.json
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
 {
     "inbounds": [
         {
@@ -41,4 +40,4 @@ cat << EOF > /usr/local/etc/Xray/config.json
 EOF
 
 # Run XRay
-/usr/local/bin/Xray -config /usr/local/etc/Xray/config.json
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
