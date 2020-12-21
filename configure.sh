2b74d547402834e4c2414e461dde13a8fa9e5e07
x-potato <50793719+yxhit@users.noreply.github.com>
2020-12-21 17:25:46 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 # Download and install V2Ray
 mkdir /tmp/xray
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/$RELEASE_VERSION/Xray-linux-$MACHINE.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/latest/Xray-linux-64.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
 install -m 755 /tmp/xray/xray /usr/local/bin/xray
 install -m 755 /tmp/xray/xctl /usr/local/bin/xctl
