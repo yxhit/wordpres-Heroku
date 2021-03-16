bead24ff471c81fe54a6b2bb6907ce89d5465cc1
x-potato <50793719+yxhit@users.noreply.github.com>
2021-03-16 17:53:55 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 # Download and install xRay
 mkdir /tmp/xray
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/v1.3.1/Xray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/v1.4.0/Xray-linux-64.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
 install -m 755 /tmp/xray/xray /usr/local/bin/xray
 
